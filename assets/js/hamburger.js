bdc4a75e5111a5ddb1e81d106bb9f54c0de90c61
abhinavreddy05 <117709452+abhinavreddy05@users.noreply.github.com>
2023-05-19 08:13:51 +0530

    tooltip in only mobile view

--- assets/js/hamburger.js
+++ assets/js/hamburger.js
@@ -2,6 +2,13 @@ const hamburger = document.getElementById("hamburger");
 const linktxt = document.getElementsByClassName("linktxt");
 const logo = document.getElementById("logo");
 const logoMobile = document.getElementById("logo-mobile");
+const navlist = document.getElementsByTagName("nav")[0].getElementsByTagName("a")
+
+if(window.matchMedia("(max-width: 900px)").matches){
+    for (let i = 0; i < navlist.length; i++) {
+        navlist[i].classList.add("hint--right")
+    }
+}
 
 hamburger.addEventListener("click", ()=>{
     var prop
@@ -22,13 +29,20 @@ hamburger.addEventListener("click", ()=>{
     }
 })
 
-
 window.addEventListener("resize", function() {
     if (window.matchMedia("(min-width: 900px)").matches) {
         logo.style.removeProperty("display")
         logoMobile.style.removeProperty("display")
         for (let i = 0; i < linktxt.length; i++) {
             linktxt[i].style.removeProperty("display");
+        }
+        for (let i = 0; i < navlist.length; i++) {
+            navlist[i].classList.remove("hint--right")
+        }
+    }
+    if(window.matchMedia("(max-width: 900px)").matches){
+        for (let i = 0; i < navlist.length; i++) {
+            navlist[i].classList.add("hint--right")
         }
     }
 })
